da50a6cdce65447dc9f370341fe06fb525074a28
= <=>
2022-07-01 17:57:22 +0400

    asa

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+ 
+   User.create(user_name: 'Emanuel', password: 'mariam', email: "admin@admin.ad", role: '2')
+
