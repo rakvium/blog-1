85397b2ac606e1268073beb42d7847305f11367d
= <=>
2022-07-14 10:03:34 +0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,11 +22,4 @@
        )
    end
 
-   (1..5).each do |id|
-    Comment.create!(
-        body: Faker::Lorem.sentence(word_count: 1),
-        user_id:1,
-        article_id: 52,
-        status:'public'
-    )
-end
+   
