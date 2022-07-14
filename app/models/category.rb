6637f55d2c8df2fb886cba20bcdf2e2b77792845
= <=>
2022-07-14 16:14:08 +0400

    added category things

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
   has_many :article_categories, dependent: :destroy
   has_many :articles, through: :article_categories
+
+  validates :name, uniqueness: true
+
 end
