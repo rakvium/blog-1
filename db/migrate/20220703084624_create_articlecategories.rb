c1dd1a1be564d6b1112c03ba83f2d8e6c21c4b99
= <=>
2022-07-14 10:44:35 +0400

    categories

--- db/migrate/20220703084624_create_articlecategories.rb
+++ db/migrate/20220703084624_create_articlecategories.rb
@@ -1,6 +1,13 @@
 class CreateArticlecategories < ActiveRecord::Migration[7.0]
   def change
-    create_table :articlecategories do |t|
+    create_table :categories do |t|
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+  def change
+    create_table :article_categories do |t|
       t.references :article, null: false, foreign_key: true
       t.references :category, null: false, foreign_key: true
 
--- db/migrate/20220704162305_droptablearticlecat.rb
+++
@@ -1,5 +0,0 @@
-class Droptablearticlecat < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :articlecategories
-  end
-end
