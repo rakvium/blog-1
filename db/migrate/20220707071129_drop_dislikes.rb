3e5f74f130a4c64bd2f3436a08967f7d6aa8ef75
= <=>
2022-07-14 10:48:44 +0400

    dislike don't work

--- db/migrate/20220707071129_drop_dislikes.rb
+++
@@ -1,5 +0,0 @@
-class DropDislikes < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :dislikes
-  end
-end
--- db/migrate/20220707071706_rename_dislikeable.rb
+++
@@ -1,8 +0,0 @@
-class RenameDislikeable < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :dislikes, :article_id, :dislikeable_id
-
-    add_index :dislikes, [:dislikeable_id, :dislikeable_type]
-
-  end
-end
